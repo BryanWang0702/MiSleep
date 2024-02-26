4a42f6b9f1eb54ea13581e006e7f4f56b8a909d1
Bryan <451583969@qq.com>
2024-02-26 19:01:45 +0800

    update signal_io with load mat, load edf, write mat

--- MiSleep/io/signal_io.py
+++ MiSleep/io/signal_io.py
@@ -4,20 +4,13 @@
 @File: signal_io.py
 @Author: Xueqiang Wang
 @Date: 2024/2/21
-@Description:  Load data from data file and write data into file
-                Support .mat, .edf, .npz currently
-
-                Load functions will return the signal data and the name for each channel:
-                signal_data, channel_names, sf
-
-                Write functions require a n-D data array for input and
-                a list for channel name (optional)
+@Description:   Load data from .mat or .edf file, write data into .mat file
+                Data contains: Signals, channel name, sampling frequency
 """
 
 from hdf5storage import loadmat, savemat
 import pyedflib
 import datetime
-import numpy as np
 
 
 def load_mat(data_path):
@@ -31,42 +24,50 @@ def load_mat(data_path):
 
     Returns
     -------
-    signal_data : ndarray
-        Data in n-D array, each dimension is one row (channel) data
+    signal_data : list
+        List of n arrays, each dimension is one row (channel) data
     channel_name : list
         Name for each channel
+    sf : list
+        Sampling frequency of each channel
     """
 
     raw_data = list(loadmat(data_path).values())[-1]
     try:
-        signals = raw_data['signals'][0, 0]
-        channels = [item for each in raw_data['channels'][0][0][0] for item in each]
+        # Whether save with python, because python has no 'cell' type
+        _ = raw_data['save']
+        channels = list(raw_data['channels'][0, 0])
         channels = [each.strip() for each in channels]
-    except Exception:
-        # If matlab data is not a struct, or have no channel field, will arise this
-        signals = raw_data
-        channels = [f'ch{each+1}' for each in range(raw_data.shape[0])]
+        sf = list(raw_data['sf'][0, 0][0].astype(float))
+        signals = [raw_data[each][0, 0][0] for each in channels]
 
-        print("We recommend to save signals in the 'signals' field, "
-              "and each signal's name in the 'channels' field")
-    if signals.shape[0] > signals.shape[1]:
-        signals = signals.T
+        return signals, channels, sf
+    except ValueError:
+        try:
+            channels = [item for each in raw_data['channels'][0][0][0] for item in each]
+            channels = [each.strip() for each in channels]
+            sf = list(raw_data['sf'][0, 0][0].astype(float))
+            signals = [raw_data[each][0, 0][0] for each in channels]
+        except Exception:
+            # If matlab data is not a struct, or have no channel field, will arise this
+            signals = raw_data
+            channels = [f'ch{each + 1}' for each in range(raw_data.shape[0])]
+            sf = [256 for _ in range(raw_data.shape[0])]
 
-    if len(channels) > signals.shape[0]:
-        channels = channels[:signals.shape[0]]
-    elif len(channels) < signals.shape[0]:
-        for i in range(len(channels), signals.shape[0]):
-            channels.append(f'ch{i+1}')
+            print("We recommend to save signals in different fields and "
+                  "channel name/sample frequency save respectively")
 
-    return signals, channels
+        return signals, channels, sf
 
 
-def write_mat(signals, channels, mat_file=None):
+def write_mat(signals, channels, sf, mat_file=None):
     """
     Write data to .mat file
 
     Parameters
     ----------
+    sf : list
+        List of int, sampling frequency of each channel in the signal data
     signals : n-D array
     channels : list
     mat_file : str
@@ -80,12 +81,10 @@ def write_mat(signals, channels, mat_file=None):
     if mat_file is None:
         mat_file = f"./{datetime.datetime.now().strftime('%Y%m%d_%H%M%S')}_saved.mat"
 
-    mdict = {
-        'data': {
-            'signals': signals,
-            'channels': channels,
-        }
-    }
+    mdict = {'data': dict(zip(channels, signals))}
+    mdict['data']['save'] = 'python'
+    mdict['data']['channels'] = channels
+    mdict['data']['sf'] = sf
 
     savemat(file_name=mat_file, mdict=mdict, format='5')
 
@@ -105,87 +104,11 @@ def load_edf(data_path):
         Data in n-D array, each dimension is one row (channel) data
     channel_name : list
         Name for each channel
+    sf : list
+        List of sampling frequency for each channel
     """
 
     signals, signal_headers, _ = pyedflib.highlevel.read_edf(edf_file=data_path)
 
-    return np.array(signals), [each['label'] for each in signal_headers]
-
-
-def write_edf(signals, channels, sf, edf_file=None):
-    """
-    Write data to .edf file
-
-    Parameters
-    ----------
-    sf : int
-        Sampling frequency
-    signals :
-    channels :
-    edf_file :
-
-    Returns
-    -------
-
-    """
-    if edf_file is None:
-        edf_file = f"./{datetime.datetime.now().strftime('%Y%m%d_%H%M%S')}_saved.edf"
-
-    # Construct the header with channels information
-    signal_headers = [{
-            'label': ch,
-            'dimension': 'uV',
-            'sample_rate': sf,
-            'physical_max': 5000,
-            'physical_min': -5000,
-            'digital_max': 32767,
-            'digital_min': -32768,
-            'transducer': '',
-            'prefilter': ''
-        } for ch in channels]
-
-    pyedflib.highlevel.write_edf(edf_file=edf_file, signals=signals,
-                                 signal_headers=signal_headers)
-
-
-def load_npz(data_path):
-    """
-    Load data from .npz file, which was stored with numpy
-    Why npz not npy? We need to store signals and channels respectively, .npz can do this
-    Parameters
-    ----------
-    data_path :
-
-    Returns
-    -------
-
-    """
-
-    npy_data = np.load(data_path)
-    try:
-        signals = npy_data['signals']
-        channels = npy_data['channels']
-
-        return signals, channels
-    except KeyError:
-        print('Ensure your .npz data was stored by MiSleep')
-        return
-
-
-def write_npz(signals, channels, npz_file=None):
-    """
-    Write data into .npz file
-    Parameters
-    ----------
-    signals :
-    channels :
-    npz_file :
-
-    Returns
-    -------
-
-    """
-    if npz_file is None:
-        npz_file = f"./{datetime.datetime.now().strftime('%Y%m%d_%H%M%S')}_saved.npz"
-
-    np.savez(npz_file, signals=signals, channels=channels)
+    return signals, [each['label'] for each in signal_headers], \
+        [each['sample_frequency'] for each in signal_headers]
--- MiSleep/io/test/test_signal_io.py
+++ MiSleep/io/test/test_signal_io.py
@@ -5,54 +5,34 @@
 @Author: Xueqiang Wang
 @Date: 2024/2/22
 @Description:
-
-Issue 20240222
-The sampling frequency for each channel could be different, so the sampling frequency information
-should be stored in the data file as a list, just like the channel name... Inspired from EDF file,
-there is a list for samples.
 """
 
 import unittest
-from MiSleep.io.signal_io import load_mat, load_edf, load_npz, \
-    write_npz, write_edf, write_mat
+from MiSleep.io.signal_io import load_mat, load_edf, write_mat
 from time import time
 
 
 class TestSignalIO(unittest.TestCase):
-    def test_load_mat_write_edf(self):
-        """Test load_mat and write_edf"""
-        mat_path = r'../../datasets/test_mat.mat'
+    def test_load_mat_write_mat(self):
+        """Test load_mat and write_mat"""
+        mat_path = r'../../datasets/mat_from_mat.mat'
         s = time()
-        signals, channels = load_mat(data_path=mat_path)
+        signals, channels, sf = load_mat(data_path=mat_path)
         e = time()
         print(f"Load data from .mat file, cost {e-s} seconds\n"
-              f"Signals shape {signals.shape}, channels: {channels}")
+              f"Signals shape {len(signals)}, channels: {channels}, sampling frequency: {sf}")
 
-        write_edf(signals, channels, 305, '../../datasets/test_edf.edf')
-        print('Saved to .edf successfully')
-
-    def test_load_edf_write_npz(self):
-        """Test load_edf and write_npz"""
-        edf_path = r'../../datasets/learn-nsrr01.edf'
-        s = time()
-        signals, channels = load_edf(data_path=edf_path)
-        e = time()
-        print(f"Load data from .edf file, cost {e-s} seconds\n"
-              f"Signals shape {signals.shape}, channels: {channels}")
-
-        write_npz(signals, channels, '../../datasets/test_npz.npz')
-        print('Saved to .npz successfully')
-
-    def test_load_npz_write_mat(self):
-        """Test load_npz and write_mat"""
-        npz_path = r'../../datasets/test_npz.npz'
-        s = time()
-        signals, channels = load_npz(data_path=npz_path)
-        e = time()
-        print(f"Load data from .npz file, cost {e-s} seconds\n"
-              f"Signals shape {signals.shape}, channels: {channels}")
-
-        write_mat(signals, channels, r'../../datasets/test_mat.mat')
+        write_mat(signals, channels, sf, r'../../datasets/mat_from_mat_.mat')
         print('Saved to .mat successfully')
 
+    def test_load_edf_write_mat(self):
+        """Test load_edf and write_mat"""
+        edf_path = r'../../datasets/learn-nsrr01.edf'
+        s = time()
+        signals, channels, sf = load_edf(data_path=edf_path)
+        e = time()
+        print(f"Load data from .edf file, cost {e-s} seconds\n"
+              f"Signals shape {len(signals)}, channels: {channels}, sampling frequency: {sf}")
 
+        write_mat(signals, channels, sf, r'../../datasets/mat_from_edf.mat')
+        print('Saved to .mat successfully')
