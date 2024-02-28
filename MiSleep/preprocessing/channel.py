e8202da3568b5919fc984d335b2493902bb776f3
Bryan <451583969@qq.com>
2024-02-28 15:08:03 +0800

    create MiData data format for MiSleep

---
+++ MiSleep/preprocessing/channel.py
@@ -0,0 +1,16 @@
+# -*- coding: UTF-8 -*-
+"""
+@Project: MiSleep_v2 
+@File: channel.py
+@Author: Xueqiang Wang
+@Date: 2024/2/28
+@Description:  Some operations for channels in the signal data
+               Include: Rename channel, channel differential
+
+               The data contains signals, channel name, channel sample frequency
+"""
+
+
+def channel_rename():
+    """Rename the channel labels"""
+
