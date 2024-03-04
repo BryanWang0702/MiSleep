1c11c54138137774bcd721e2b24031f1e9c31af4
BryanWang0702 <451583969@qq.com>
2024-03-04 14:15:00 +0800

    create spectral.py for spectrum and sprctrogram

--- MiSleep/preprocessing/spectral.py
+++ MiSleep/preprocessing/spectral.py
@@ -4,6 +4,29 @@
 @File: spectral.py
 @Author: Xueqiang Wang
 @Date: 2024/3/4
-@Description:  
+@Description:  Spectrum, time-frequency (spectrogram) analysis
 """
 
+from MiSleep.io.base import MiData
+
+
+def spectrum(midata, ch_names=None, power_band=None):
+    """
+
+    Parameters
+    ----------
+    midata : MiData
+    ch_names : list, optional
+        Default is None -> all channels
+    power_band : list, optional
+        List of frequency  bands of interests. Each tuple should contain the lower and upper frequencies,
+        as well as the band name (e.g. (0.5, 4, 'Delta'))
+    Returns
+    -------
+    freq : list
+    power : list
+    """
+    if not isinstance(midata, MiData):
+        raise TypeError(f"'midata' should be an instance of MiData, got {type(midata)}")
+    if ch_names is None or ch_names == []:
+        ch_names = midata.channels
