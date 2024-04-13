0a52d4944c7c0184f41c4fe55b4ed81120c031a6
BryanWang0702 <451583969@qq.com>
2024-04-13 10:04:33 +0800

    fix the HOUR/MINUTE revert problem in transfer result, and split out the transfer function in annotation_io file

--- MiSleep/io/annotation_io.py
+++ MiSleep/io/annotation_io.py
@@ -7,7 +7,9 @@
 @Description:  Annotation io, default is for MiSleep annotation, `NAME.txt`
 """
 from misleep.io.base import MiAnnotation
-from misleep.utils.annotation import marker2mianno, start_end2mianno, sleep_state2mianno
+from misleep.utils.annotation import marker2mianno, start_end2mianno, lst2group, sleep_state2mianno, \
+    transfer_time, insert_row, temp_loop4below_row
+import pandas as pd
 
 
 def load_misleep_anno(file_path):
@@ -44,3 +46,99 @@ def load_misleep_anno(file_path):
     sleep_state = sleep_state2mianno(sleep_state)
 
     return MiAnnotation(sleep_state=sleep_state, start_end=start_end, marker=marker)
+
+def transfer_result(mianno, ac_time):
+    marker = [[
+        transfer_time(ac_time, each[0], '%Y-%m-%d %H:%M:%S'), 
+        each[0], each[1]] for each in mianno.marker]
+
+    start_end_label = [[
+        transfer_time(ac_time, each[0], '%Y-%m-%d %H:%M:%S', ms=True), each[0], 1,
+        transfer_time(ac_time, each[1], '%Y-%m-%d %H:%M:%S', ms=True), each[1], 0,
+        each[2]
+    ] for each in mianno.start_end]
+
+    sleep_state = lst2group([[idx+1, each] 
+                                for idx, each in enumerate(mianno.sleep_state)])
+    sleep_state = [[
+        transfer_time(ac_time, each[0], '%Y-%m-%d %H:%M:%S'), each[0], 1,
+        transfer_time(ac_time, each[1], '%Y-%m-%d %H:%M:%S'), each[1], 0,
+        each[2], mianno.state_map[each[2]]
+    ] for each in sleep_state]
+
+    columns=['start_time', 'start_time_sec', 'start_code',
+                'end_time', 'end_time_sec', 'end_code',
+                'state_code', 'state']
+
+    df = pd.DataFrame(data=sleep_state, columns=columns)
+    
+    new_df = pd.DataFrame(columns=columns)
+    for idx, row in df.iterrows():
+        if row['end_time_sec'] % 3600 == 0:
+            new_df = insert_row(new_df, idx, row)
+            # Just add a row and nothing else
+            new_row = pd.Series([
+                row['end_time'], row['end_time_sec'], ' ',
+                row['end_time'], row['end_time_sec'], '5',
+                ' ', 'MARKER'
+            ], index=columns)
+            new_df = insert_row(new_df, new_df.shape[0], new_row)
+            continue
+
+        if int(row['end_time_sec'] / 3600) > int(row['start_time_sec'] / 3600):
+
+            previous_row, new_row, below_row = temp_loop4below_row(row, ac_time, columns)
+
+            new_df = insert_row(new_df, new_df.shape[0], previous_row)
+            new_df = insert_row(new_df, new_df.shape[0], new_row)
+            while int(below_row['end_time_sec'] / 3600) > int(below_row['start_time_sec'] / 3600):
+                row = below_row
+                previous_row, new_row, below_row = temp_loop4below_row(row, ac_time, columns)
+                new_df = insert_row(new_df, new_df.shape[0], previous_row)
+                new_df = insert_row(new_df, new_df.shape[0], new_row)
+
+            new_df = insert_row(new_df, new_df.shape[0], below_row)
+            continue
+
+        new_df = insert_row(new_df, new_df.shape[0], row)
+
+    df = new_df
+    del new_df
+
+    df['bout_duration'] = df.apply(
+        lambda x: x[4] - x[1] + 1 if x[7] != 'MARKER' else '', axis=1)
+    
+    df['hour'] = df['start_time_sec'].apply(lambda x: int(x / 3600) if x % 3600 != 0 else '')
+    analyse_df = pd.DataFrame()
+
+    temp_hour = list(set(list(df['hour'])))
+    temp_hour.remove('')
+    temp_hour = sorted(temp_hour)
+    analyse_df['date_time'] = [transfer_time(ac_time, each*3600, "%Y-%m-%d %H:%M:%S")
+                            for each in temp_hour]
+
+    features = []
+    for each in temp_hour:
+        df_ = df[df['hour'] == each]
+        temp_lst = []
+        for phase in ["NREM", "REM", "Wake", "INIT"]:
+            _duration = df_[df_["state"] == phase]["bout_duration"].sum()
+            _bout = df_[df_["state"] == phase]["bout_duration"].count()
+            temp_lst += [_duration, _bout, round(_duration / _bout, 2) if _bout != 0 else 0, round(_duration / 3600, 2)]
+        features.append(temp_lst)
+
+    analyse_df[['NREM_duration', 'NREM_bout', "NREM_ave", "NREM_percentage",
+                'REM_duration', 'REM_bout', "REM_ave", "REM_percentage",
+                'WAKE_duration', 'WAKE_bout', "WAKE_ave", "WAKE_percentage",
+                'INIT_duration', 'INIT_bout', "INIT_ave", "INIT_percentage"]] = features
+
+
+    analyse_df[
+        ['NREM_duration', 'NREM_bout', 'REM_duration', 'REM_bout', 'WAKE_duration',
+        'WAKE_bout', 'INIT_duration', 'INIT_bout']
+    ] = analyse_df[
+        ['NREM_duration', 'NREM_bout', 'REM_duration', 'REM_bout', 'WAKE_duration',
+        'WAKE_bout', 'INIT_duration', 'INIT_bout']].astype(int)
+    
+    return df, analyse_df
+
